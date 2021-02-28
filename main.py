f7fbb7f272a4a91ff0329bc3a579963681a34b7b
Philippe <philippebs92@hotmail.com>
2021-02-28 15:36:17 -0300

    Acerto no run do app

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ def index():
 
 #Executa nossa aplicacao
 if __name__ == "__main__":
-	app.run(debug=True, port=4000)
+    port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
 

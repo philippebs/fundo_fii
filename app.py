ecb40fae052e3684a653a6cbfcaf39d7e95fabe9
Philippe <philippebs92@hotmail.com>
2021-02-28 17:18:57 -0300

    incluindo dependencias e busca pelo site de investimento

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import os
 
 #Inicializa nossa aplicacao Flask
 app =  Flask(__name__)
@@ -6,6 +10,9 @@ app =  Flask(__name__)
 #Cria a rota para o caminho
 @app.route("/")
 def index():
+	headers = {"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 GTB7.1 (.NET CLR 3.5.30729)", "Referer": "http://example.com"}
+	url = 'https://www.fundsexplorer.com.br/ranking'
+	response = requests.get(url, headers=headers, timeout=10)
 	return render_template('index.html')
 
 #Executa nossa aplicacao
